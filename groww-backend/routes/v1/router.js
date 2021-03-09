3514154d11531d84eeac8905a106139e9e27b288
naman-gupta99 <kungfunaman@gmail.com>
2021-03-09 22:14:50 +0530

    Mutual Funds Routes Added

--- groww-backend/routes/v1/router.js
+++ groww-backend/routes/v1/router.js
@@ -1,6 +1,8 @@
+import mutualFundsRouter from "./mutual-fund";
 import stocksRouter from "./stocks";
 
 const router = (app) => {
+    app.use("/v1/mutual-funds", mutualFundsRouter);
     app.use("/v1/stocks", stocksRouter);
 };
 
