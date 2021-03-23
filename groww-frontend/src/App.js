00a3f328a329aed0d31f9f7cac48f710ac53845d
Ankit Vashisht <ankitvashisht12@gmail.com>
2021-03-23 22:05:13 +0530

    added chatbot

--- groww-frontend/src/App.js
+++ groww-frontend/src/App.js
@@ -4,13 +4,45 @@ import AuthContext from './context/AuthContext';
 import Main from './screens/Main/Main';
 import Welcome from './screens/Welcome/Welcome';
 
+
+
+import GrowwChatbot from './components/Chatbot/GrowwChatbot';
+import { ConditionallyRender } from "react-util-kit";
+import ChatBotButton from './assets/groww-logo.png';
+import './App.css';
+
+
 function App() {
   const [ isLog, toggleIsLog ]= useState(false);
+  const [showChatbot, toggleChatbot] = useState(true);
+
+
   return (
     <AuthContext.Provider value={[isLog, toggleIsLog]}>
       <Header />
       <hr />
       { isLog ? <Main /> : <Welcome />}
+      <ConditionallyRender
+            ifTrue={showChatbot}
+            show={
+              <GrowwChatbot/>
+            }
+          />
+
+      <button className='text-center' style={{
+        width: '50px', 
+        height: '50px',
+        borderRadius: '25px',
+        border: 'none',
+        position: 'fixed',
+        bottom: '15px',
+        zIndex: '9999',
+        right: '40px' 
+        }}
+          onClick={() => toggleChatbot((prev) => !prev)}
+        >
+          <img src={ChatBotButton} width='50px' height='50px' alt='chatbot logo' />
+        </button>
     </AuthContext.Provider> 
 
   );
---
+++ groww-frontend/src/components/Chatbot/ActionProvider.js
@@ -0,0 +1,9 @@
+class ActionProvider {
+  constructor(createChatBotMessage, setStateFunc, createClientMessage) {
+    this.createChatBotMessage = createChatBotMessage;
+    this.setState = setStateFunc;
+    this.createClientMessage = createClientMessage;
+  }
+}
+
+export default ActionProvider;
