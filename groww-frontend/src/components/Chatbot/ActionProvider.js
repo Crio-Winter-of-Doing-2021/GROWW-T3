1f58b30d3cf9b495a4cf2bdf5c19592f5dbcf531
naman-gupta99 <kungfunaman@gmail.com>
2021-03-25 17:18:42 +0530

    Initial Chatbot

--- groww-frontend/src/components/Chatbot/ActionProvider.js
+++ groww-frontend/src/components/Chatbot/ActionProvider.js
@@ -1,9 +1,38 @@
 class ActionProvider {
-  constructor(createChatBotMessage, setStateFunc, createClientMessage) {
-    this.createChatBotMessage = createChatBotMessage;
-    this.setState = setStateFunc;
-    this.createClientMessage = createClientMessage;
-  }
+    constructor(createChatBotMessage, setStateFunc, createClientMessage) {
+        this.createChatBotMessage = createChatBotMessage;
+        this.setState = setStateFunc;
+        this.createClientMessage = createClientMessage;
+    }
+
+    // Helper Function to update state
+    addMessageToBotState = (messages, newState) => {
+        if (Array.isArray(messages)) {
+            this.setState((state) => ({
+                ...state,
+                ...newState,
+                messages: [...state.messages, ...messages],
+            }));
+        } else {
+            this.setState((state) => ({
+                ...state,
+                ...newState,
+                messages: [...state.messages, messages],
+            }));
+        }
+    };
+
+    // Default Response
+    handleDefault = () => {
+        const message = this.createChatBotMessage(
+            "Select one of the questions below or Click on Explore more FAQs.",
+            {
+                widget: "options",
+            }
+        );
+
+        this.addMessageToBotState(message);
+    };
 }
 
 export default ActionProvider;
--- groww-frontend/src/components/Chatbot/config.js
+++ groww-frontend/src/components/Chatbot/config.js
@@ -1,19 +1,28 @@
 import { createChatBotMessage } from "react-chatbot-kit";
+import GeneralOptions from "./widgets/options/GeneralOptions/GeneralOptions";
 
-let botName = 'GrowwBot';
+let botName = "GrowwBot";
 const config = {
-	botName,
-	customStyles: {
-    botMessageBox: {
-      backgroundColor: "#00C798",
+    botName,
+    customStyles: {
+        botMessageBox: {
+            backgroundColor: "#00C798",
+        },
+        chatButton: {
+            backgroundColor: "#5367FF",
+        },
     },
-    chatButton: {
-      backgroundColor: "#5367FF",
-    },
-  },
-  initialMessages: [
-		createChatBotMessage(`Hi, I'm ${botName} 🤖. How may I help you ?`)
-	]
-}
+    initialMessages: [
+        createChatBotMessage(`Hi, I'm ${botName} 🤖. How may I help you ?`, {
+            widget: "options",
+        }),
+    ],
+    widgets: [
+        {
+            widgetName: "options",
+            widgetFunc: (props) => <GeneralOptions {...props} />,
+        },
+    ],
+};
 
-export default config
+export default config;
