f27bdb8b7eb4cbdcc591e2b42999bd1f987ee327
PratyushGarg2222 <pratyush.garg2222@gmail.com>
2021-04-06 23:52:51 +0530

    Added fetch request for stocks

--- groww-frontend-withrouter/src/Screens/Stocks/stocks.js
+++ groww-frontend-withrouter/src/Screens/Stocks/stocks.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import stocksDetails from '../../fixtures/stocks.json';
 import StockItem from '../../Components/StockItem/StockItem';
 import Classes from './stocks.module.css';
 
-function Stocks(){
+const Stocks = () => {
+
+    let stocks = [];
+
+    useEffect(() => {
+
+        fetch('https://groww-bot-backend.herokuapp.com/v1/stocks')
+            .then(r=>r.json())
+            .then(data => {
+               console.log(data);
+               stocks = data;
+            });
+
+    });
 
     return (
         <div className = "container">
             Stocks Page
             <div className = "row">
-                {stocksDetails.exploreCompanies.MOST_VALUABLE.map(stock => <StockItem stock={stock} />)}
+                {stocksDetails.exploreCompanies.MOST_VALUABLE.map(stock => <StockItem stock={stock} key = {stock.company.isin}/>)}
             </div>
         </div>
-    )
+    );
 
 }
 
