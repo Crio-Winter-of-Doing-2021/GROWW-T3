3094cbd41c1beea433514803c94062d9562e7226
naman-gupta99 <kungfunaman@gmail.com>
2021-04-08 00:43:22 +0530

    Fixed Infinite loop issue

--- groww-frontend-withrouter/src/Screens/Stocks/stocks.js
+++ groww-frontend-withrouter/src/Screens/Stocks/stocks.js
@@ -1,32 +1,36 @@
-import React, {useEffect, useState} from 'react';
-import stocksDetails from '../../fixtures/stocks.json';
-import StockItem from '../../Components/StockItem/StockItem';
-import Classes from './stocks.module.css';
+import React, { useEffect, useState } from "react";
+import stocksDetails from "../../fixtures/stocks.json";
+import StockItem from "../../Components/StockItem/StockItem";
+import Classes from "./stocks.module.css";
 
 const Stocks = (props) => {
-
     const [stocks, setStocks] = useState([]);
 
     useEffect(() => {
-
-        fetch('https://groww-bot-backend.herokuapp.com/v1/stocks')
-            .then(r=>r.json())
-            .then(data => {
-               console.log(data);
-               setStocks(data);
+        fetch("https://groww-bot-backend.herokuapp.com/v1/stocks")
+            .then((r) => r.json())
+            .then((data) => {
+                console.log(data);
+                setStocks(data);
             });
-
-    });
+    }, []);
 
     return (
-        <div className = "container">
+        <div className="container">
             Stocks Page
-            <div className = "row">
-                {stocks ? stocks.map(stock => <StockItem stock={stock} key = {stock.id} updChatbot = {props.updChatbot} />) : null}
+            <div className="row">
+                {stocks
+                    ? stocks.map((stock) => (
+                          <StockItem
+                              stock={stock}
+                              key={stock.id}
+                              updChatbot={props.updChatbot}
+                          />
+                      ))
+                    : null}
             </div>
         </div>
     );
-
-}
+};
 
 export default Stocks;
